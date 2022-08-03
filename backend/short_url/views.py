dc27fbcf98a094710326c9d12c10fb5905c4abcd
Max Vu <max.vu.goldenowl@gmail.com>
2022-08-03 14:50:48 +0700

    Refactor code

--- backend/short_url/views.py
+++ backend/short_url/views.py
@@ -1,33 +1,19 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from django.views.decorators.csrf import csrf_exempt, ensure_csrf_cookie
+from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse, Http404, HttpResponseRedirect
 import json
 
-from .serializers import ShortenerSerializer
 from .models import Shortener
-from .utils import create_shortened_url
-
-class ShortenerView(viewsets.ModelViewSet):
-  queryset = Shortener.objects.all().only('original_url', 'short_url')
-  ShortenerSerializer(queryset, many=True, fields = ('original_url', 'short_url')).data
 
 @csrf_exempt
 def save_url_view(request):
   if request.method == 'POST':
     json_data = json.loads(request.body.decode('UTF-8'))
-    short_url_object = Shortener.objects.filter(original_url=json_data['original_url'])
-    if short_url_object:
-      data = {
-        'original_url': short_url_object[0].original_url,
-        'short_url': request.build_absolute_uri('/') + short_url_object[0].short_url
-      }
-    else:
-      short_url_object = Shortener.objects.create(original_url=json_data['original_url'])
-      data = {
-        'original_url': short_url_object.original_url,
-        'short_url': request.build_absolute_uri('/') + short_url_object.short_url
-      }
+    short_url_object, created = Shortener.objects.get_or_create(original_url=json_data['original_url'])
+
+    data = {
+      'original_url': short_url_object.original_url,
+      'short_url': request.build_absolute_uri('/') + short_url_object.short_url
+    }
 
     return JsonResponse(data)
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -45,10 +45,7 @@ class App extends Component {
   isUrlValid = () => {
     const { originalUrl } = this.state;
     const res = originalUrl.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
-    if (res == null)
-      return false;
-    else
-      return true;
+    return !!res;
   }
 
   render() {
